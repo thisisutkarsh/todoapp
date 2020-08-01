f93eac016331690eff61dfea516ae02d601908b1
thisisutkarsh <kr.utkarsh777@gmail.com>
2020-08-01 22:31:24 +0530

    Updates

---
+++ precache-manifest.8cffc4fee117bbe8a68fe3f7245a1977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d16f7ea3dd4bb7c763d0e851b44f3ca",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "2e9cc0f95e5eb927f1e1",
+    "url": "/todoapp/static/css/main.1755549f.chunk.css"
+  },
+  {
+    "revision": "c2a75fdb1d756b746294",
+    "url": "/todoapp/static/js/2.e7a8ebaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.e7a8ebaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9cc0f95e5eb927f1e1",
+    "url": "/todoapp/static/js/main.c2ed6d14.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
